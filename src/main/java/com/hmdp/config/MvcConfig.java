84005b07ca9c36362bcbaef941dcc98dab50bfb7
Alex <1586924294@qq.com>
2022-05-19 17:53:43 +0800

    redis实现登录注册、商品缓存

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,30 @@
+package com.hmdp.config;
+
+import com.hmdp.utils.LoginInterceptor;
+import com.hmdp.utils.RefreshTokenInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).order(0);
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/voucher/**",
+                        "/shop/**",
+                        "shop-type/**",
+                        "/blog/hot",
+                        "/user/code",
+                        "/user/login",
+                        "/upload/**"
+                ).order(1);
+    }
+}
